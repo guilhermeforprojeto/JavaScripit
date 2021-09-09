d519bb27c0863a1d20fda275a375cf605b9a6715
guilhermeforprojeto <guylhermesantos@hotmail.com>
2021-09-09 10:27:14 -0300

    Stilo e - Link para paginas - Treinando e personalizando

---
+++ gyncursos/scripit/cadastro.js
@@ -0,0 +1,15 @@
+
+
+
+var startPage = confirm("Iniciar cadastro?")
+    if(startPage == true){
+
+                
+        var nome        =   prompt("Digite seu 1º Nome","Digite aqui")
+        var sobremone   =   prompt("Digite seu Sobrenome","Digite aqui")
+        document.write("Olá "+nome+sobremone+", Obrigado por usar nossos testes de JS")
+
+
+    }else{
+        window.location.replace("./index.html");
+    }
