9687896e78e76b6c96d3c4e5b3095b8612b16afd
guilhermeforprojeto <guylhermesantos@hotmail.com>
2021-09-09 08:54:36 -0300

    Trabalhando com variaveis

--- gyncursos/home.js
+++ gyncursos/home.js
@@ -3,16 +3,17 @@ var nome        =   prompt("Digite seu 1º Nome","Digite aqui")
 var sobremone   =   prompt("Digite seu Sobrenome","Digite aqui")
 var mensagem
 var retorno     =   confirm("Iniciar calculo de adição?")
+
+
     if(retorno == true){
-
-                alert("Calculo teste!")
-        let a           =   prompt("Digite 1 numero inteiro ","")
-        let b           =   prompt("Novamente digite outro/igual ","")
-        let resultado   =   Number(a)+Number(b)
-        document.write("Olá "+nome+""+sobremone+"A soma de "+a+" e "+b+" é: "+resultado)
-
-    }else{
-        document.write("Operação foi cancelada, obrigado por usar nossos testes de JS")
+        
+            var a           =   prompt("Digite 1 numero inteiro ")
+            var b           =   prompt("Novamente digite outro/igual ")
+            var resultado   =   Number(a)+Number(b)
+            document.write("Olá "+nome+""+sobremone+"A soma de "+a+" e "+b+" é: "+resultado)
+        
+    }else{  
+        document.write("Olá "+nome+sobremone+". Operação foi cancelada, obrigado por usar nossos testes de JS")
     }
 
 
