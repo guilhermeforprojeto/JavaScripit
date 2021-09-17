c874d93a280aedd8e5b62790d81afba9a01106ad
guilhermeforprojeto <guylhermesantos@hotmail.com>
2021-09-17 16:38:18 -0300

    Array basico

---
+++ gyncursos/scripit/array.js
@@ -0,0 +1,4 @@
+var Cidades = ["São Paulo", "Santo André","Diadema"]
+for (i = 0; i < 3; i++){
+    document.write(Cidades[i] + "<br/>")
+}
--- gyncursos/scripit/loopWhile.js
+++ gyncursos/scripit/loopWhile.js
@@ -1,4 +1,4 @@
-var iniciar = confirm("Proto para iniciar o loop While, ele vai repetir dentro dacondição o numero de vezes que você informar")
+var iniciar = confirm("Proto para iniciar o loop While, ele vai repetir dentro da condição o numero de vezes que você informar")
 if (iniciar == true){
 var numero, contador = 0
 numero = prompt("While! Digite um numero menor entre 10 e 1000")
