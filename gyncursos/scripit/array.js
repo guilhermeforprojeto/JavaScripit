dcf669750fb9bea79c3d2e855c1b55e05501a27c
guilhermeforprojeto <guylhermesantos@hotmail.com>
2021-09-24 09:41:32 -0300

    Array de numeros ordenados

--- gyncursos/scripit/array.js
+++ gyncursos/scripit/array.js
@@ -1,4 +1,13 @@
+alert("Vai começar")
 var Cidades = ["São Paulo", "Santo André","Diadema"]
 for (i = 0; i < 3; i++){
-    document.write(Cidades[i] + "<br/>")
+    document.write("Cidade ",Cidades[i] + "<br/>")
+}
+document.write("<br/>","Nomes em ordem","<br/>","<br/>")
+
+
+
+Cidades.sort()
+for (i = 0; i < Cidades.length; i++){
+    document.write("Cidade ",Cidades[i] + "<br/>")
 }
