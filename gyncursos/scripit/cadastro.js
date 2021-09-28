85b2bd89780c97060b7df1187a4c6bee05c48a6e
guilhermeforprojeto <guylhermesantos@hotmail.com>
2021-09-28 17:17:53 -0300

    Passando valor para parametro da function

--- gyncursos/scripit/cadastro.js
+++ gyncursos/scripit/cadastro.js
@@ -6,7 +6,7 @@ var startPage = confirm("Iniciar cadastro?")
                 
         var nome        =   prompt("Digite seu 1º Nome","Digite aqui")
         var sobremone   =   prompt("Digite seu Sobrenome","Digite aqui")
-        document.write("Olá "+nome+sobremone+", Obrigado por usar nossos testes de JS")
+        document.write("Olá "+nome + sobremone +", Obrigado por usar nossos testes de JS"+"<br/>"+"<br/>")
 
 
     }else{
@@ -20,3 +20,17 @@ function Mensagem(aniversariante){
 
 //chamando a função
 Mensagem(nome)
+
+function valorReferencia(strVar,arrVar){
+    strVar      = "String Modificada"
+    arrVar[0]   = "Array modificado"
+}
+
+var texto = "Valor Original"
+var vetor = ["Valor Oiginal"]
+document.write("String antes de chamar a função:"+ texto + "<br/>")
+document.write("Array antes de chamar a função"+ vetor[0]+"<br/>")
+//chamar a função
+valorReferencia(texto, vetor);
+document.write("String após chamar a função: " + texto + "<br/>")
+document.write("Array apos chamar a função: " + vetor[0] + "<br/>")
