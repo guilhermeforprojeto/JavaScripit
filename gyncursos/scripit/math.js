e7fd8cccdb393ec526d65af8519f2532fe60af15
guilhermeforprojeto <guylhermesantos@hotmail.com>
2021-09-29 19:25:35 -0300

    Adicionei o Math no calculo.html

---
+++ gyncursos/scripit/math.js
@@ -0,0 +1,11 @@
+alert("Vamos ver aqui o uso de Math")
+
+var a = 2.56
+var b = 4
+var c = 3
+
+document.write("<br>"+"Arredondar para cima")
+document.write("<br>"+"o Numero " + a + ", Virou "+ Math.ceil(a) +"<br>")
+document.write("A variavel a elevado b elevada a variavel c ("+b +" e "+c+") é igual "+ Math.pow(b,c)+"<br>" )  
+document.write("O menor valor entre as 3 variaveis é "+Math.min(a,b,c)+"<br>")
+document.write("Gerando um valor aleatorio 2 vezes 1ª "+Math.random()+" 2º "+Math.random()) 
