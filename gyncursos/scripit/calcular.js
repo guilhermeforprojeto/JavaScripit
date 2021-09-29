fefa0efd00434744ff6628edf9544ac6a82336e8
guilhermeforprojeto <guylhermesantos@hotmail.com>
2021-09-28 21:39:07 -0300

    Funções – Retornando valores com a Instrução return

--- gyncursos/scripit/calcular.js
+++ gyncursos/scripit/calcular.js
@@ -1,4 +1,4 @@
-var retorno     =   confirm("Iniciar calculo de adição?")
+var retorno     =   confirm("Começar pagina de calculos programados?")
 
 
     if(retorno == true){
@@ -6,13 +6,10 @@ var retorno     =   confirm("Iniciar calculo de adição?")
             var a           =   prompt("Digite 1 numero inteiro ")
             var b           =   prompt("Novamente digite outro/igual ")
             var resultado   =   Number(a)+Number(b)
-            document.write("Olá a soma de "+a+" e "+b+" é: "+resultado)
-        
-    }else{ 
-        window.location.replace("./index.html");
-     }
+            document.write("Olá a soma de "+a+" e "+b+" é: "+resultado + "<br/>")
+   
 
-alert("Vamos aqui usando function, calcular a corrente eletrica")
+alert("Agora vamos aqui usando function, calcular a corrente eletrica")
 
 var V = prompt("Digite o valor da tensão")
 var R = prompt("Digite o valor da resistencia")
@@ -23,4 +20,20 @@ function calcula(V,R){
 }
 
 var corrente = calcula(V,R)
-document.write("O valor da corrent é", corrente )
+document.write("O valor da corrent é ", corrente + "<br/>" ) 
+
+    function quadrado(numero){
+            if (isNaN(numero)){
+                return "Digite um nuuumero valido!"
+            }
+            quad = numero * numero
+            return quad
+    }
+
+    var num = prompt("Entre com um numero qualquer")
+    document.write("O Quadrado de "+ num +" é "+ quadrado(num))
+
+        
+    }else{ 
+        window.location.replace("./index.html");
+    }
